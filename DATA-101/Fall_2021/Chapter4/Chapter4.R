fb62b1e8128d608a9f34d09a606adbcace4bb620
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-08-18 23:14:03 +0000

    Adding Euclidean distance

---
+++ DATA-101/Fall_2021/Chapter4/Chapter4.R
@@ -0,0 +1,10 @@
+######################
+## Chapter 4 R-code ##
+######################
+
+## First we declare the vectors u and v
+u = c(23, 2, 2)
+v = c(40, 10, 1)
+
+## Here we compute the Euclidean distance
+sqrt(sum((u - v)^2))

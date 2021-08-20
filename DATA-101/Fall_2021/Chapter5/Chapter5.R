136cbd48636331b2a8acec70c2bf22432d8fb37b
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-08-20 21:47:10 +0000

    Adding multiple linear regression

--- DATA-101/Fall_2021/Chapter5/Chapter5.R
+++ DATA-101/Fall_2021/Chapter5/Chapter5.R
@@ -48,3 +48,31 @@ summary(md)
 plot(fitted(md), residuals(md), xlab = 'TV', ylab = 'Residuals')
 grid()
 abline(h = 0, lwd = 2)
+
+
+###############
+## R-squared ##
+###############
+
+## Here we read the data 
+advertising = read.csv(file = 'Advertising.csv')
+
+## Here we fit the simple linear model
+md = lm(Sales ~ TV, data = advertising)
+
+## Here we print the model summary results
+summary(md)$r.squared
+
+
+################################
+## Multiple Linear Regression ##
+################################
+
+## Here we read the data 
+advertising = read.csv(file = 'Advertising.csv')
+
+## Here we fit the multiple linear model
+md = lm(Sales ~ TV + Radio + Newspaper, data = advertising)
+
+## Here we print the model summary results
+summary(md)

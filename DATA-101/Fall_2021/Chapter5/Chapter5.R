3ce82ecbae339b83c5f486a2fe2c56b2902e0b64
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-08-20 21:44:33 +0000

    linear assumption

--- DATA-101/Fall_2021/Chapter5/Chapter5.R
+++ DATA-101/Fall_2021/Chapter5/Chapter5.R
@@ -2,9 +2,18 @@
 ## Chapter 5 Code ##
 ####################
 
-#######################
-## Linear Regression ##
-#######################
+## Here we read the data 
+advertising = read.csv(file = 'Advertising.csv')
+
+## Here we fit the simple linear model
+md = lm(Sales ~ TV, data = advertising)
+
+## Here we print the model summary results
+summary(md)
+
+################################
+## Checking linear assumption ##
+################################
 
 ## Here we read the data 
 advertising = read.csv(file = 'Advertising.csv')
@@ -14,3 +23,8 @@ md = lm(Sales ~ TV, data = advertising)
 
 ## Here we print the model summary results
 summary(md)
+
+## Here we check for linearity by creating the scatter plot
+plot(advertising$TV, residuals(md), xlab = 'TV', ylab = 'Residuals')
+grid()
+abline(h = 0, lwd = 2)

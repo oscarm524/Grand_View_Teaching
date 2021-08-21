a29f37dfdba4c8297d2e53d415cb0dbfec26d42b
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-08-21 17:14:44 +0000

    Adding backward selection

--- DATA-101/Fall_2021/Chapter6/Chapter6.R
+++ DATA-101/Fall_2021/Chapter6/Chapter6.R
@@ -21,3 +21,18 @@ upper_md = lm(mpg ~ cylinders + displacement + horsepower + weight +
 forward_selection = step(null_md, 
                          scope = list(lower = null_md, upper = upper_md), 
                          direction = "forward")
+
+
+########################
+## Backward Selection ##
+########################
+
+## Here we read the data into R
+auto = read.csv(file = 'auto.csv')
+
+## Here we define the full model 
+full_md = lm(mpg ~ cylinders + displacement + horsepower + weight + 
+                   acceleration, data = auto)
+
+## Here we perform backward selection
+backward_selection = step(full_md, direction = 'backward') 

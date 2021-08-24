2dd02f4ba9adba9311864c05ac0a7232ffb782db
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-08-24 06:00:17 +0000

    adding histogram to r-script

--- DATA-437/Fall_2021/Chapter2/Chapter2.R
+++ DATA-437/Fall_2021/Chapter2/Chapter2.R
@@ -13,3 +13,28 @@ table(NY$season_2011)
 
 ## Creating a relative frequency table
 prop.table(table(NY$season_2011))
+
+################
+## Histograms ##
+################
+
+## Histogram of Jammael Charles' rushing attempts in 2010
+charles = read.csv(file = 'Dataset_2_2.csv')
+
+hist(charles$Yards, col = 'gray', xlab = 'Yards', ylab = 'Frequency')
+box()
+
+## Histogram of shooting percentage
+shooting = read.csv(file = 'Dataset_2_3.csv')
+
+hist(shooting$SPCT, col = 'gray', xlab = 'Shooting Percentage', ylab = 'Frequency')
+box()
+
+## Histogram of guards
+hist(shooting$SPCT[shooting$Pos == 'G'], col = 'gray', xlab = 'Shooting Percentage', ylab = 'Frequency')
+box()
+
+## Histogram of forwards
+hist(shooting$SPCT[shooting$Pos %in% c('SF', 'PF')], col = 'gray', xlab = 'Shooting Percentage', ylab = 'Frequency')
+box()
+

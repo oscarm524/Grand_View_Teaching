a5600bdda81ab006c2de9e6e4d3def7475e4d2b3
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-09-01 19:30:45 +0000

    R-script is ready to go!

--- DATA-101/Fall_2021/In_Class_Assignments/Section_A/In_Class_3/In_Class_3.R
+++ DATA-101/Fall_2021/In_Class_Assignments/Section_A/In_Class_3/In_Class_3.R
@@ -1,1 +1,11 @@
-## Reading csv file
+## Reading the csv file 
+autos = read.csv(file = 'Automobile_data.csv')
+
+## Reporting the price of the last observation
+tail(autos, 1)
+
+## Loading plyr
+library(plyr)
+
+body_type_max = ddply(autos, .(body.style), summarise, max_price = max(price, na.rm = T))
+body_type_max

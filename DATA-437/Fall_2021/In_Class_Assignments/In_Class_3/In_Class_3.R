05579924eeaa35ccb3e46592ca45cce54e32d45a
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-09-01 19:24:40 +0000

    in-class 3 R-script ready to go!

--- DATA-437/Fall_2021/In_Class_Assignments/In_Class_3/In_Class_3.R
+++ DATA-437/Fall_2021/In_Class_Assignments/In_Class_3/In_Class_3.R
@@ -1,1 +1,15 @@
 ## Reading csv file
+goalie_stats = read.csv(file = 'game_goalie_stats.csv')
+head(goalie_stats)
+
+## Loading plyr 
+library(plyr)
+
+## Counting the number of games of each player
+player_games = ddply(goalie_stats, .(player_id), summarise, numb_of_games = length(unique(game_id)))
+player_games
+
+## Creating a histogram of the number of games
+hist(player_games$numb_of_games, col = 'gray', xlab = 'Number of Games')
+box()
+

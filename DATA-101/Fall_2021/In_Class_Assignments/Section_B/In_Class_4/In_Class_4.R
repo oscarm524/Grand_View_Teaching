e474f7c8406534b4e55178a7d56c9a34538f3347
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-09-07 18:02:04 +0000

    adding answers

---
+++ DATA-101/Fall_2021/In_Class_Assignments/Section_B/In_Class_4/In_Class_4.R
@@ -0,0 +1,12 @@
+## Creating the age vector 
+age = c(60, 61, 62, 63, 64, 65, 66, 68, 68, 69)
+
+## Computing the mean age
+mean(age)
+
+## Computing the variance of age
+var(age)
+
+## Computing the mean of the log(age)
+mean(log(age))
+

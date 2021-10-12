fa904c79edf815fc813df504d175a7296c640f63
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-10-12 04:24:35 +0000

    adding quadratic regression

--- DATA-437/Fall_2021/Chapter6/Chapter6.R
+++ DATA-437/Fall_2021/Chapter6/Chapter6.R
@@ -11,3 +11,13 @@ lm_md = lm(Runs ~ OPS, data = baseball)
 
 ## Extracting model results 
 summary(lm_md)
+
+## Reading the csv file 
+ken = read.csv(file = 'Dataset_6_4.csv')
+head(ken)
+
+## Fitting quadratic model to Ken's home runs data
+quadratic_md = lm(HR ~ T + I(T^2), data = ken)
+
+## Extracting model results
+summary(quadratic_md)

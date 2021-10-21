f1ca086f396c2915bebbad9d4865a40387b483ff
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-10-21 14:25:22 +0000

    adding logistic model

--- DATA-101/Fall_2021/In_Class_Assignments/Section_B/In_Class_13/In_Class_13.R
+++ DATA-101/Fall_2021/In_Class_Assignments/Section_B/In_Class_13/In_Class_13.R
@@ -1,2 +1,9 @@
 ## Reading the csv file
 churn = read.csv(file = 'Customer_Churn.csv')
+
+## Creating Churn_numb
+churn$Churn_numb = ifelse(churn$Churn == 'No', 0, 1)
+
+## Building the logistic regression model 
+logit_md = glm(Churn_numb ~ tenure + MonthlyCharges, data = churn, 
+               family = binomial(link = logit))

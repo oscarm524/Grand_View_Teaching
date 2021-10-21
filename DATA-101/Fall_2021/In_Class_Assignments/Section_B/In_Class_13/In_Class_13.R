9c7754b6140f6dcdc8c3bb84efcf9643564136f0
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-10-21 14:26:05 +0000

    updating model

--- DATA-101/Fall_2021/In_Class_Assignments/Section_B/In_Class_13/In_Class_13.R
+++ DATA-101/Fall_2021/In_Class_Assignments/Section_B/In_Class_13/In_Class_13.R
@@ -5,8 +5,7 @@ churn = read.csv(file = 'Customer_Churn.csv')
 churn$Churn_numb = ifelse(churn$Churn == 'No', 0, 1)
 
 ## Building the logistic regression model 
-logit_md = glm(Churn_numb ~ tenure + MonthlyCharges, data = churn, 
-               family = binomial(link = logit))
+logit_md = glm(Churn_numb ~ tenure + MonthlyCharges, data = churn, family = binomial(link = logit))
 
 ## Predicting the churn of customer 
 ## Tenure = 12, MonthlyCharges = 250

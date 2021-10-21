23157d215da69b830f70119a4cc34cf55b50a1a1
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-10-21 14:25:49 +0000

    adding model prediction

--- DATA-101/Fall_2021/In_Class_Assignments/Section_B/In_Class_13/In_Class_13.R
+++ DATA-101/Fall_2021/In_Class_Assignments/Section_B/In_Class_13/In_Class_13.R
@@ -7,3 +7,12 @@ churn$Churn_numb = ifelse(churn$Churn == 'No', 0, 1)
 ## Building the logistic regression model 
 logit_md = glm(Churn_numb ~ tenure + MonthlyCharges, data = churn, 
                family = binomial(link = logit))
+
+## Predicting the churn of customer 
+## Tenure = 12, MonthlyCharges = 250
+new_obs = data.frame(tenure = 12, MonthlyCharges = 250)
+
+## Predicting churn 
+predict(logit_md, newdata = new_obs, type = 'response')
+
+

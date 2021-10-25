6b71804694a76b59ca899fbb18cf7f2e4702a054
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-10-25 18:07:28 +0000

    adding model

--- DATA-101/Fall_2021/In_Class_Assignments/Section_A/In_Class_14/In_Class_14.R
+++ DATA-101/Fall_2021/In_Class_Assignments/Section_A/In_Class_14/In_Class_14.R
@@ -5,3 +5,8 @@ head(churn)
 ## Changing labels to 0-1
 churn$Churn_0_1 = ifelse(churn$Churn == 'No', 0, 1)
 head(churn)
+
+library(rpart)
+
+## Building the decision tree model 
+tree_md = rpart(Churn_0_1 ~ tenure + MonthlyCharges, data = churn)

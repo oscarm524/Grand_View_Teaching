c114565bf2e69b1cd0f8826d42e4b87f7a05a356
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-10-27 18:14:09 +0000

    adding modeling

--- DATA-437/Fall_2021/In_Class_Assignments/In_Class_16/In_Class_16.R
+++ DATA-437/Fall_2021/In_Class_Assignments/In_Class_16/In_Class_16.R
@@ -1,3 +1,10 @@
 ## Reading the csv file 
 nba = read.csv(file = 'NBA_2006_2007.csv')
 head(nba)
+
+## Building the linear regression model 
+lm_md = lm(W ~ EFG_pct + FTA_rate + TOV_pct + OREB_pct, data = nba)
+
+## Extracting model results
+summary(lm_md)
+

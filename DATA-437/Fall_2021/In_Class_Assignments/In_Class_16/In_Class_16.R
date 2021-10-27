811a363360400ed20f83119628f19ca18e727b5f
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-10-27 18:14:46 +0000

    adding model prediction

--- DATA-437/Fall_2021/In_Class_Assignments/In_Class_16/In_Class_16.R
+++ DATA-437/Fall_2021/In_Class_Assignments/In_Class_16/In_Class_16.R
@@ -8,3 +8,9 @@ lm_md = lm(W ~ EFG_pct + FTA_rate + TOV_pct + OREB_pct, data = nba)
 ## Extracting model results
 summary(lm_md)
 
+## Defining the new observation 
+newdata = data.frame('EFG_pct' =  51, 'FTA_rate' =  0.35, 'TOV_pct' = 16, 'OREB_pct' = 32)
+
+## Estimating the wins 
+predict(lm_md, newdata, type = 'response')
+

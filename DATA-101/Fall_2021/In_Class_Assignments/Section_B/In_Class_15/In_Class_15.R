5beaeebfd5b05e3e31bc0bb516265b276c79ef2d
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-10-28 22:02:08 +0000

    adding random forest model

--- DATA-101/Fall_2021/In_Class_Assignments/Section_B/In_Class_15/In_Class_15.R
+++ DATA-101/Fall_2021/In_Class_Assignments/Section_B/In_Class_15/In_Class_15.R
@@ -1,3 +1,8 @@
 ## Changing labels to 0-1
 churn$Churn_numb = ifelse(churn$Churn == 'No', 0, 1)
 head(churn)
+
+library(randomForest)
+
+## Building the random forest model
+RF_md = randomForest(as.factor(Churn_numb) ~ tenure + MonthlyCharges, data = churn)

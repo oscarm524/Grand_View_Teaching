85328b7222111bdfd32e0402ff457333016787a6
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-11-01 21:10:14 +0000

    defining the null model

--- DATA-101/Fall_2021/In_Class_Assignments/Section_A/In_Class_16/In_Class_16.R
+++ DATA-101/Fall_2021/In_Class_Assignments/Section_A/In_Class_16/In_Class_16.R
@@ -5,3 +5,7 @@ head(churn)
 ## Changing labels to 0-1
 churn$Churn_numb = ifelse(churn$Churn == 'No', 0, 1)
 head(churn)
+
+## Defining the null model 
+md_null = glm(Churn_numb ~ 1, data = churn, family = 'binomial')
+summary(md_null)

ed72c5573bc5fb9fb3f7b2370bd4482ee2f435d5
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-11-03 20:20:10 +0000

    defining the full model

--- DATA-101/Fall_2021/In_Class_Assignments/Section_A/In_Class_17/In_Class_17.R
+++ DATA-101/Fall_2021/In_Class_Assignments/Section_A/In_Class_17/In_Class_17.R
@@ -6,3 +6,6 @@ head(churn)
 churn$Churn_numb = ifelse(churn$Churn == 'No', 0, 1)
 head(churn)
 
+## Defining the full model 
+md_full = glm(Churn_numb ~ gender + SeniorCitizen + tenure + Contract + PaperlessBilling + MonthlyCharges + DeviceProtection + TechSupport, data = churn, family = 'binomial')
+summary(md_full)

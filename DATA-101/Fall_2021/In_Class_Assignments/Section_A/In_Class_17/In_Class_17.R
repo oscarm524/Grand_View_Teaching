aa7a053b05de731429cbeae94747e299cb3b81f0
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-11-03 20:19:42 +0000

    changing labels to numbers

--- DATA-101/Fall_2021/In_Class_Assignments/Section_A/In_Class_17/In_Class_17.R
+++ DATA-101/Fall_2021/In_Class_Assignments/Section_A/In_Class_17/In_Class_17.R
@@ -1,3 +1,8 @@
 ## Reading the csv file 
 churn = read.csv(file = 'Customer_Churn.csv')
 head(churn)
+
+## Changing labels to 0-1
+churn$Churn_numb = ifelse(churn$Churn == 'No', 0, 1)
+head(churn)
+

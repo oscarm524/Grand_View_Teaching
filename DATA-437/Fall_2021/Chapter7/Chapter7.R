d06cb0a36c4dd82b91e17b6400cdfac3f6553d81
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-11-05 17:18:19 +0000

    adding computation of RD and Wpct

--- DATA-437/Fall_2021/Chapter7/Chapter7.R
+++ DATA-437/Fall_2021/Chapter7/Chapter7.R
@@ -5,3 +5,8 @@ head(teams)
 ## Selecting variables and seasons of interest
 my_teams = subset(teams, yearID > 2000, select = c(teamID, yearID, lgID, G, W, L, R, RA))
 head(my_teams)
+
+## Computing RD and Wpct
+my_teams$RD = my_teams$R - my_teams$RA
+my_teams$Wpct = my_teams$W / (my_teams$W + my_teams$L)
+head(my_teams)

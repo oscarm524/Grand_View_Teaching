f2991e111046f900c452301b59bcef19ec70a163
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-11-05 17:05:28 +0000

    adding data subset and variable selection

--- DATA-437/Fall_2021/Chapter7/Chapter7.R
+++ DATA-437/Fall_2021/Chapter7/Chapter7.R
@@ -1,3 +1,7 @@
 ## Reading the csv file
 teams = read.csv(file = 'Teams.csv')
 head(teams)
+
+## Selecting variables and season of interest
+my_teams = subset(teams, yearID > 2000, select = c(teamID, yearID, lgID, G, W, L, R, RA))
+head(my_teams)

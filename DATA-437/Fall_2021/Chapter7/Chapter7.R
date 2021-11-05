740b5bbc4a63ff72fa9cd7f4307407d0d49206bd
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-11-05 17:06:26 +0000

    updating comment

--- DATA-437/Fall_2021/Chapter7/Chapter7.R
+++ DATA-437/Fall_2021/Chapter7/Chapter7.R
@@ -2,6 +2,6 @@
 teams = read.csv(file = 'Teams.csv')
 head(teams)
 
-## Selecting variables and season of interest
+## Selecting variables and seasons of interest
 my_teams = subset(teams, yearID > 2000, select = c(teamID, yearID, lgID, G, W, L, R, RA))
 head(my_teams)

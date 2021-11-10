959c9bbe6f2ce74dcfcda6409c76bc59e2e3a80b
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-11-10 19:05:23 +0000

    adding gradient boosting

--- DATA-101/Fall_2021/In_Class_Assignments/Section_A/In_Class_19/In_Class_19.R
+++ DATA-101/Fall_2021/In_Class_Assignments/Section_A/In_Class_19/In_Class_19.R
@@ -45,3 +45,18 @@ RF_pred = predict(RF_md, test)
 ## Computing the RMSE and MAE
 RMSE_RF = sqrt(mean((RF_pred - test$mpg)^2))
 MAE_RF = mean(abs(RF_pred - test$mpg))
+
+#######################
+## Gradient Boosting ##
+#######################
+
+library(gbm)
+
+GB_md = gbm(mpg ~ z_cylinders + z_displacement + z_horsepower + z_weight + z_acceleration, data = train, n.tree = 500, interaction.depth = 4, distribution = 'gaussian')
+
+## Predicting on the test dataset 
+GB_pred = predict(GB_md, test, n.tree = 500)
+
+## Computing the RMSE and MAE
+RMSE_GB = sqrt(mean((GB_pred - test$mpg)^2))
+MAE_GB = mean(abs(GB_pred - test$mpg))

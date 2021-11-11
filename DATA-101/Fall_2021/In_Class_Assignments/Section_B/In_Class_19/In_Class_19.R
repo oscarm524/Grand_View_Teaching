093f68f4ae168f11287871772424e7d6696dc0ff
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-11-11 17:09:04 +0000

    adding k-nn model

--- DATA-101/Fall_2021/In_Class_Assignments/Section_B/In_Class_19/In_Class_19.R
+++ DATA-101/Fall_2021/In_Class_Assignments/Section_B/In_Class_19/In_Class_19.R
@@ -15,3 +15,18 @@ train_obs = sample(1:n, round(0.8*n))
 
 train = autos[train_obs, ]
 test = autos[-train_obs, ]
+
+##########
+## k-NN ##
+##########
+
+library(caret)
+
+knn_md = knnreg(mpg ~ z_cylinders + z_displacement + z_horsepower + z_weight + z_acceleration, data = train, k = 5)
+
+## Predicting on the test dataset 
+knn_pred = predict(knn_md, test)
+
+## Computing the RMSE and MAE
+RMSE_knn = sqrt(mean((knn_pred - test$mpg)^2))
+MAE_knn = mean(abs(knn_pred - test$mpg))

c93b4d9bea0a80d246e2f3e26525875874c5fac5
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-11-11 17:08:29 +0000

    splitting data into train and test

--- DATA-101/Fall_2021/In_Class_Assignments/Section_B/In_Class_19/In_Class_19.R
+++ DATA-101/Fall_2021/In_Class_Assignments/Section_B/In_Class_19/In_Class_19.R
@@ -8,3 +8,10 @@ autos$z_displacement = (autos$displacement - mean(autos$displacement)) / sd(auto
 autos$z_horsepower = (autos$horsepower - mean(autos$horsepower)) / sd(autos$horsepower)
 autos$z_weight = (autos$weight - mean(autos$weight)) / sd(autos$weight)
 autos$z_acceleration = (autos$acceleration - mean(autos$acceleration)) / sd(autos$acceleration)
+
+## Splitting the data into training and testing 
+n = dim(autos)[1]
+train_obs = sample(1:n, round(0.8*n))
+
+train = autos[train_obs, ]
+test = autos[-train_obs, ]

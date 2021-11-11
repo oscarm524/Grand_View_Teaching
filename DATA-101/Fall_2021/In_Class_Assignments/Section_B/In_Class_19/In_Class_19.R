01766d69f77f1ad18ce919b99858b20acc879a2f
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-11-11 17:07:59 +0000

    adding data standardization

--- DATA-101/Fall_2021/In_Class_Assignments/Section_B/In_Class_19/In_Class_19.R
+++ DATA-101/Fall_2021/In_Class_Assignments/Section_B/In_Class_19/In_Class_19.R
@@ -1,3 +1,10 @@
 ## Reading the csv file
 autos = read.csv(file = 'autos.csv')
 head(autos)
+
+## Standardizing the input data 
+autos$z_cylinders = (autos$cylinders - mean(autos$cylinders)) / sd(autos$cylinders)
+autos$z_displacement = (autos$displacement - mean(autos$displacement)) / sd(autos$displacement)
+autos$z_horsepower = (autos$horsepower - mean(autos$horsepower)) / sd(autos$horsepower)
+autos$z_weight = (autos$weight - mean(autos$weight)) / sd(autos$weight)
+autos$z_acceleration = (autos$acceleration - mean(autos$acceleration)) / sd(autos$acceleration)

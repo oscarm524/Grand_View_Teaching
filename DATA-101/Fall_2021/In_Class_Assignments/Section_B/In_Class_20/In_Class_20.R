27e8c48a97a0dcbab4bb7038c5ca15d7dd64ae6d
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-11-16 19:02:34 +0000

    splitting data

--- DATA-101/Fall_2021/In_Class_Assignments/Section_B/In_Class_20/In_Class_20.R
+++ DATA-101/Fall_2021/In_Class_Assignments/Section_B/In_Class_20/In_Class_20.R
@@ -1,3 +1,10 @@
 ## Reading the csv file
 autos = read.csv(file = 'autos.csv')
 head(autos)
+
+## Splitting the data 
+n = dim(autos)[1]
+
+train_index = sample(1:n, round(0.8*n))
+train = autos[train_index, ]
+test = autos[-train_index, ]

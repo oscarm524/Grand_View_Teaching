1c7ede33cc7e6d7ff6a6e7a97b54ea94da9cbb2b
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-11-16 19:02:59 +0000

    defining trees and depth

--- DATA-101/Fall_2021/In_Class_Assignments/Section_B/In_Class_20/In_Class_20.R
+++ DATA-101/Fall_2021/In_Class_Assignments/Section_B/In_Class_20/In_Class_20.R
@@ -8,3 +8,10 @@ n = dim(autos)[1]
 train_index = sample(1:n, round(0.8*n))
 train = autos[train_index, ]
 test = autos[-train_index, ]
+
+## Defining the number of trees 
+n_tree = c(100, 300, 500, 800, 1000, 1500, 2000)
+
+## Defining the depth of the trees
+n_depth = c(2, 3, 4, 5, 6)
+

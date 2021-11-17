d51ab83bc31c16d99ee45b58b67bdcd29c7afacc
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2021-11-17 17:15:37 +0000

    defining the trees

--- DATA-101/Fall_2021/In_Class_Assignments/Section_A/In_Class_20/In_Class_20.R
+++ DATA-101/Fall_2021/In_Class_Assignments/Section_A/In_Class_20/In_Class_20.R
@@ -8,3 +8,8 @@ n = dim(autos)[1]
 train_index = sample(1:n, round(0.8*n))
 train = autos[train_index, ]
 test = autos[-train_index, ]
+
+library(gbm)
+
+## Defining the number of trees
+n_tree = c(100, 300, 500, 800, 1000, 1500, 2000)

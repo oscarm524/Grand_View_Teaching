6fb828f308fb7d0f54bc54bbf8fb815c0eaa4b0f
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-02-09 01:28:35 +0000

    updating function

--- DATA-448/Spring_2022/In_Class_Assignments/In_Class_7/Help_Fun.py
+++ DATA-448/Spring_2022/In_Class_Assignments/In_Class_7/Help_Fun.py
@@ -4,4 +4,10 @@ from sklearn.metrics import precision_recall_curve
 
 def precision_recall_cutoff(Y_test, Y_pred):
     
+    ## Computing the precision recall curve
+    precision, recall, thresholds = precision_recall_curve(Y_test, Y_pred)
+    
+    ## Creating the precision-recall data-frame
+    precision_recall = pd.DataFrame('precision': precision, 'recall': recall, 'cutoff': thresholds)
+    
     

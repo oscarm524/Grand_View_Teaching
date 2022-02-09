b0d59828b8a736b8bc030840f9d36298d448117d
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-02-09 01:31:54 +0000

    adding code

--- DATA-448/Spring_2022/In_Class_Assignments/In_Class_7/Help_Fun.py
+++ DATA-448/Spring_2022/In_Class_Assignments/In_Class_7/Help_Fun.py
@@ -10,4 +10,8 @@ def precision_recall_cutoff(Y_test, Y_pred):
     ## Creating the precision-recall data-frame
     precision_recall = pd.DataFrame('precision': precision, 'recall': recall, 'cutoff': thresholds)
     
+    ## Finding the optimal cutoff (closest to precision = 1, recall = 1)
+    precision_recall['1_minus_precision'] = 1 - precision_recall['precision']
+    precision_recall['1_minus_recall'] = 1 - precision_recall['recall']
+    
     

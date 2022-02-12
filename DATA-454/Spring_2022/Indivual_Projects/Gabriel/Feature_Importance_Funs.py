61fda7f2220e93d65558774e87ac146ab6b4cdbf
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-02-12 22:38:48 +0000

    adding library

--- DATA-454/Spring_2022/Indivual_Projects/Gabriel/Feature_Importance_Funs.py
+++ DATA-454/Spring_2022/Indivual_Projects/Gabriel/Feature_Importance_Funs.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import StratifiedKFold
+from sklearn.ensemble import RandomForestClassifier
 
 
 def RF_Feature_Importance(X, Y, nsim, cv):

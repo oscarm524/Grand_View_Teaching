d1560eb896478def8e12a334ac3231cfcc9e394c
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-02-18 15:37:04 +0000

    updating function

--- DATA-454/Spring_2022/Indivual_Projects/Gabriel/dmc_2014_optimal_cutoff.py
+++ DATA-454/Spring_2022/Indivual_Projects/Gabriel/dmc_2014_optimal_cutoff.py
@@ -2,3 +2,11 @@ import pandas as pd
 import numpy as np
 
 
+def dmc2014_cutoff(Y_true, Y_pred):
+    
+    """
+    
+    This function estimate the optimal cutoff value
+    based on the evaluation function. 
+    
+    """

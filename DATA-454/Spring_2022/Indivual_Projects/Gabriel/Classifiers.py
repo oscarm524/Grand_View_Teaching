b12bdc0910a43f54839b910cd12eb4b8034dcc16
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-02-18 17:31:48 +0000

    updating ada-boost

--- DATA-454/Spring_2022/Indivual_Projects/Gabriel/Classifiers.py
+++ DATA-454/Spring_2022/Indivual_Projects/Gabriel/Classifiers.py
@@ -102,56 +102,39 @@ def Classifier(X_train, Y_train, X_val, Y_val, model):
         ###############################################
 
         ## Number of trees
-        n_estimators = [100, 500, 1000, 1500, 2000]
+        n_estimators = [100, 300, 500]
 
         ## Maximum number of levels in tree
-        max_depth = [5, 7, 10]
+        max_depth = [3, 5, 7]
 
         ## Learning rate
         learning_rate = [0.001, 0.01, 0.1, 0.5, 1]
 
-        ## Defining cutoffs values
-        cutoffs = [round(x, 2) for x in np.linspace(start = 0.1, stop = 0.5, num = 5)]
-
         ## Creating the dictionary of hyper-parameters
         param_grid = {'n_estimators': n_estimators,
-        'max_depth': max_depth,
-        'learning_rate': learning_rate}
+                      'max_depth': max_depth,
+                      'learning_rate': learning_rate}
 
         param_grid = expand_grid(param_grid)
 
         ## Adding accuracy and recall columns
-        param_grid['cutoff'] = np.nan
-        param_grid['accuracy'] = np.nan
-        param_grid['recall'] = np.nan
+        param_grid['evaluation'] = np.nan
 
         for i in range(param_grid.shape[0]):
 
             ## Fitting the model (using the ith combination of hyper-parameters)
-            Ada_md = AdaBoostClassifier(base_estimator = DecisionTreeClassifier(max_depth = param_grid['max_depth'][i]),
-            n_estimators = param_grid['n_estimators'][i],
-            learning_rate = param_grid['learning_rate'][i])
+            Ada_md = AdaBoostClassifier(base_estimator = DecisionTreeClassifier(max_depth =                                                            param_grid['max_depth'][i]),
+                                        n_estimators = param_grid['n_estimators'][i],
+                                        learning_rate = param_grid['learning_rate'][i])
 
             Ada_md.fit(X_train, Y_train)
 
             ## Predicting on the val dataset
             preds = Ada_md.predict_proba(X_val)[:, 1]
 
-            ## Extracting False-Positive, True-Positive and optimal cutoff
-            False_Positive_Rate, True_Positive_Rate, cutoff = roc_curve(Y_val, preds)
-
-            ## Finding optimal cutoff (the one that maximizes True-Positive and minimizes False-Positive)
-            to_select = np.argmax(True_Positive_Rate - False_Positive_Rate)
-            opt_cutoff = cutoff[to_select]
-
-            ## Changing to 0-1
-            Y_hat = np.where(preds <= opt_cutoff, 0, 1)
-
-            ## Computing accuracy and recall
-            param_grid.iloc[i, 3] = opt_cutoff
-            param_grid.iloc[i, 4] = accuracy_score(Y_val, Y_hat)
-            param_grid.iloc[i, 5] = recall_score(Y_val, Y_hat, average = 'macro')
-
+            ## Computing prediction evaluation (based on 2014 dmc)
+            param_grid.iloc[i, 3] = np.sum(abs(Y_val - preds))
+            
         return param_grid
 
 

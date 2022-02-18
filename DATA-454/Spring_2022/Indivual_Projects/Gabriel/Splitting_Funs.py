38855b9bfe6a54b776fd5fd676e9db6a578ced0d
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-02-18 01:44:25 +0000

    function ready to go!

--- DATA-454/Spring_2022/Indivual_Projects/Gabriel/Splitting_Funs.py
+++ DATA-454/Spring_2022/Indivual_Projects/Gabriel/Splitting_Funs.py
@@ -10,3 +10,9 @@ def train_validation_test(X, Y):
     """
     
     ## Initinal split
+    X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size = 0.2, stratify = Y)
+    
+    ## Second split 
+    X_val, X_test, Y_val, Y_test = train_test_split(X_val, Y_val, test_size = 0.5, stratify = Y_val)
+    
+    return [X_train, X_val, X_test, Y_train, Y_val, Y_test]

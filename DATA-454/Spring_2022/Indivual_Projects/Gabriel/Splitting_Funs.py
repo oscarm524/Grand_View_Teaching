50741f2efa30156dac69399bc62c0584f648a0f7
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-02-18 01:42:09 +0000

    updating function

--- DATA-454/Spring_2022/Indivual_Projects/Gabriel/Splitting_Funs.py
+++ DATA-454/Spring_2022/Indivual_Projects/Gabriel/Splitting_Funs.py
@@ -2,4 +2,11 @@ from sklearn.model_selection import train_test_split
 
 def train_validation_test(X, Y):
     
+    """
+    This function splits the data into train (80%),
+    validation (10%) and test (10%)
+    X: input variables
+    Y: target variable
+    """
     
+    ## Initinal split

6c9f2f6183eddb34e6d670439b2a7047e7a962fa
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-02-25 15:32:04 +0000

    adding expand grid fucntion

--- DATA-454/Spring_2022/Indivual_Projects/Gabriel/Ensemble.py
+++ DATA-454/Spring_2022/Indivual_Projects/Gabriel/Ensemble.py
@@ -3,6 +3,10 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 
 
+def expand_grid(dictionary):
+    return pd.DataFrame([row for row in product(*dictionary.values())], columns = dictionary.keys())
+
+
 def ensemble_gabriel_ricky(test_pred1, test_pred2, test_pred3, Y, to_score1, to_score2, to_score3):
     
     ## Defining the input variables 

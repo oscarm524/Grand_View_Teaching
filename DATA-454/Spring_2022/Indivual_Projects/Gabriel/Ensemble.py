9252890586dc8d237fa4c525eb27e084f54ce0ea
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-02-25 17:40:18 +0000

    adding function/library

--- DATA-454/Spring_2022/Indivual_Projects/Gabriel/Ensemble.py
+++ DATA-454/Spring_2022/Indivual_Projects/Gabriel/Ensemble.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from itertools import product
 
 
 def expand_grid(dictionary):

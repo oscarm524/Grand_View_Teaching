1222f64830601d7156aa0a9bcf12cbe96cb0730a
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-02-25 15:25:51 +0000

    adding initial function

--- DATA-454/Spring_2022/Indivual_Projects/Gabriel/Ensemble.py
+++ DATA-454/Spring_2022/Indivual_Projects/Gabriel/Ensemble.py
@@ -1,3 +1,6 @@
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifer, GradientBoostingClassifier
+
+
+def ensemble_gabriel_ricky(pred1, pred2, pred3, )

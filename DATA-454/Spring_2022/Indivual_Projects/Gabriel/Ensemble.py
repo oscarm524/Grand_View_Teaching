92f602a767aa898f23e11d0b2dab7aa7b871d0bf
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-02-25 15:27:34 +0000

    adding ahlyn funciton

--- DATA-454/Spring_2022/Indivual_Projects/Gabriel/Ensemble.py
+++ DATA-454/Spring_2022/Indivual_Projects/Gabriel/Ensemble.py
@@ -3,4 +3,12 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifer, GradientBoostingClassifier
 
 
-def ensemble_gabriel_ricky(pred1, pred2, pred3, )
+def ensemble_gabriel_ricky(test_pred1, test_pred2, test_pred3, Y, to_score1, to_score2, to_score3):
+    
+    
+    
+    
+    
+    
+    
+def ensemble_ashlyn(test_pred1, test_pred2, test_pred3, Y, to_score1, to_score2, to_score3):

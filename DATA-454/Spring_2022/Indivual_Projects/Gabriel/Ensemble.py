f718166d2f88235de529f23ade668de9baac6893
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-02-25 15:31:29 +0000

    updating function

--- DATA-454/Spring_2022/Indivual_Projects/Gabriel/Ensemble.py
+++ DATA-454/Spring_2022/Indivual_Projects/Gabriel/Ensemble.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifer, GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 
 def ensemble_gabriel_ricky(test_pred1, test_pred2, test_pred3, Y, to_score1, to_score2, to_score3):
+    
+    ## Defining the input variables 
+    X_input = pd.concat([test_pred1, test_pred2, test_pred3], axis = 1)
+    X_to_score = pd.concat([to_score1, to_score2, to_score3], axis = 1)
     
     
     

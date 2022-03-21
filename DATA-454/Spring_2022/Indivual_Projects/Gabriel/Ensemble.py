cdb99c9ed67ed45ef37e7ae8660925f93a45e367
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-03-21 16:30:18 +0000

    adding confusion_matrix

--- DATA-454/Spring_2022/Indivual_Projects/Gabriel/Ensemble.py
+++ DATA-454/Spring_2022/Indivual_Projects/Gabriel/Ensemble.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
 from itertools import product
 
 

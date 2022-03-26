ad1b2598321c0a1c9ad5caf3f470f20a62d8416a
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-03-26 17:25:51 +0000

    cost function script is ready to go!

--- DATA-448/Spring_2022/In_Class_Assignments/In_Class_17/cost_function.py
+++ DATA-448/Spring_2022/In_Class_Assignments/In_Class_17/cost_function.py
@@ -28,6 +28,25 @@ def cost_function(Y_true, Y_pred):
     
     return results['cost'][0]
 
-my_score_function = make_scorer(cost_function, greater_is_better = True, needs_proba = True)
+
+def cost_function_cutoff(Y_true, Y_pred):
+    
+    ## Defining cutoff values in a data-frame
+    results = pd.DataFrame({'cutoffs': np.round(np.linspace(0.05, 0.95, num = 40, endpoint = True), 2)})
+    results['cost'] = np.nan
+    
+    for i in range(0, results.shape[0]):
+        
+        ## Changing likelihoods to labels
+        Y_pred_lab = np.where(Y_pred < results['cutoffs'][i], 0, 1)
+        
+        ## Computing confusion matrix and scoring based on description
+        X = confusion_matrix(Y_pred_lab, Y_true)
+        results['cost'][i] = -1500 * X[1, 0] - 1000 * X[0, 1] + 500 * X[1, 1]
+        
+    ## Sorting results 
+    results = results.sort_values(by = 'cost', ascending = False).reset_index(drop = True)
+    
+    return results['cutoffs'][0]
 
 

eccab61374b2a29280cd0a5ba73f546d5fa97cbd
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-05-22 13:36:24 +0000

    adding box-plot code

--- DATA-101/Fall_2022/Chapter3/Chapter3.R
+++ DATA-101/Fall_2022/Chapter3/Chapter3.R
@@ -83,4 +83,25 @@ colors = c('gold', 'yellowgreen', 'lightcoral', 'lightskyblue')
 ## Here we create the pie chart using the pie function
 pie(passengers, labels = classes, col = colors)
 
+## Here we create the vector of stock prices
+stock = c(11.88, 6.27, 5.49, 4.81, 4.40, 3.78, 3.44, 3.11, 2.88, 2.68,
+           7.99, 6.07, 5.26, 4.79, 4.05, 3.69, 3.36, 3.03, 2.74, 2.63,
+           7.15, 5.98, 5.07, 4.55, 3.94, 3.62, 3.26, 2.99, 2.74, 2.62,
+           7.13, 5.91, 4.94, 4.43, 3.93, 3.48, 3.20, 2.89, 2.69, 2.61)
+       
+## Here we create the histogram of stock prices using
+## the hist function       
+hist(stock, col = 'gray', main = 'Stock Price', ylab = 'Number of Shares', 
+     xlab = 'Price ($)')
+
+## Here we create the vector of stock prices
+stock = c(11.88, 6.27, 5.49, 4.81, 4.40, 3.78, 3.44, 3.11, 2.88, 2.68,
+           7.99, 6.07, 5.26, 4.79, 4.05, 3.69, 3.36, 3.03, 2.74, 2.63,
+           7.15, 5.98, 5.07, 4.55, 3.94, 3.62, 3.26, 2.99, 2.74, 2.62,
+           7.13, 5.91, 4.94, 4.43, 3.93, 3.48, 3.20, 2.89, 2.69, 2.61)
+       
+## Here we create the box-plot of stock prices using
+## the boxplot function   
+boxplot(stock, ylab = 'Stock Prices ($)')    
+
 

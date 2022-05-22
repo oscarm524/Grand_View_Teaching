c534b8009b48cb662b75313c149d5769767f0063
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-05-22 13:24:34 +0000

    adding mean code

--- DATA-101/Fall_2022/Chapter3/Chapter3.R
+++ DATA-101/Fall_2022/Chapter3/Chapter3.R
@@ -3,3 +3,9 @@ x = c(0, 1, 2, 0, 4, 0, 1, 2, 3)
 
 ## Here we find the mean of the vector x
 mean(x)
+
+## First we create the a vector that contains the salaries
+salaries = c(35000, 37000, 35000, 33000, 210000)
+
+## Here we find the mean of the vector salaries
+mean(salaries)

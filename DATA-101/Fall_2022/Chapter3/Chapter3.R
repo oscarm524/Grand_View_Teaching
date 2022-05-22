fe00a18f6a27abcdd8425ca1f6922f98fc085430
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-05-22 13:29:04 +0000

    adding range code

--- DATA-101/Fall_2022/Chapter3/Chapter3.R
+++ DATA-101/Fall_2022/Chapter3/Chapter3.R
@@ -27,3 +27,11 @@ salaries = c(35000, 37000, 35000, 33000, 210000, 39000)
 
 ## Here we find the median of the vector salaries
 median(salaries)
+
+## First we create the a vector that contains the salaries
+salaries = c(35000, 37000, 35000, 33000, 210000)
+
+## Here we find the range of the vector salaries using the max and min functions
+max(salaries) - min(salaries)
+
+

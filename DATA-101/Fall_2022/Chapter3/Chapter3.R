4474ced46eb386f7a4765fa9d4b5b1f5b32b13cf
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-05-22 13:30:38 +0000

    adding variance and std

--- DATA-101/Fall_2022/Chapter3/Chapter3.R
+++ DATA-101/Fall_2022/Chapter3/Chapter3.R
@@ -34,4 +34,13 @@ salaries = c(35000, 37000, 35000, 33000, 210000)
 ## Here we find the range of the vector salaries using the max and min functions
 max(salaries) - min(salaries)
 
+## First we create the a vector that contains the heights
+heights = c(65.71, 72.30, 68.31, 67.05, 70.68)
+
+## Here we find the variance of heights 
+var(heights)
+
+## Here we find the standard deviation of heights
+sd(heights)
+
 

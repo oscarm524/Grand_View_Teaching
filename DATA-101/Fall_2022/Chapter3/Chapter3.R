82d2647dead81e40e3120b40d5ccd451c935e4f4
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-05-22 13:28:05 +0000

    updating script

--- DATA-101/Fall_2022/Chapter3/Chapter3.R
+++ DATA-101/Fall_2022/Chapter3/Chapter3.R
@@ -21,3 +21,9 @@ salaries = c(35000, 37000, 35000, 33000, 210000)
 
 ## Here we find the median of the vector salaries
 median(salaries)
+
+## First we create the a vector that contains the salaries
+salaries = c(35000, 37000, 35000, 33000, 210000, 39000)
+
+## Here we find the median of the vector salaries
+median(salaries)
